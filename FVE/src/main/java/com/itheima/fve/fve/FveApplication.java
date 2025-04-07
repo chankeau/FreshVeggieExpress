44805b99b0f2a82ea72096039fdf8c5abbab8496
chankeau <824330126@qq.com>
2025-04-07 16:09:14 +0800

    初始项目

---
+++ FVE/src/main/java/com/itheima/fve/fve/FveApplication.java
@@ -0,0 +1,14 @@
+package com.itheima.fve.fve;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j//日志
+@SpringBootApplication
+public class FveApplication {
+    public static void main(String[] args){
+        SpringApplication.run(FveApplication.class,args);
+        log.info("项目启动成功...");
+    }
+}
---
+++ FVE/src/main/java/com/itheima/fve/fve/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.itheima.fve.fve.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Slf4j
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    /**
+     * 设置静态资源映射（指定访问页面路径）-static
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //添加映射
+        log.info("映射资源开始");
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
